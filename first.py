1a84fd2e63d554861ca26d815759dd798f310dab
Ilya <ilyarogashev@gmail.com>
2023-11-21 19:01:50 +0400

    Coffee table

--- first.py
+++ first.py
@@ -1,5 +1,30 @@
-def main():
-    print('My first git-repo')
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class DBSample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.table.setColumnCount(7)
+        self.table.setRowCount(0)
+        res = self.connection.cursor().execute("SELECT * FROM Coffee").fetchall()
+        for i, row in enumerate(res):
+            self.table.setRowCount(
+                self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = DBSample()
+    ex.show()
+    sys.exit(app.exec())
